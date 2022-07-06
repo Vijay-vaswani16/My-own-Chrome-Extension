e32e147098a377454c028453b08e3be060c67ff0
Vijay-vaswani16 <77836274+Vijay-vaswani16@users.noreply.github.com>
2022-07-06 14:46:05 +0530

    Add files via upload

---
+++ my own extension/content-script.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    console.log('viijay');
+    // IDEExecute_submit__btn__1-QBY
+    // const clickEvent = document.getElementsByClassName('');
+    // clickEvent.addEventListener('click', () => { printSomething(); });
+    document.getElementsByClassName("IDEExecute_submit__btn__1-QBY")[0].addEventListener("click",function() {console.log('bijay');});
+}
+
+// chrome.runtime.onMessage.addListener((message) => {
+//     /*
+//     * query DOM to get problem name and code
+//     * user sendResponse method to send info back to the sender
+//     * as a JSON object.
+//     */
+//     });
